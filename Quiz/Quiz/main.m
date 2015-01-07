6940cf87eb481de2cb59d666b02f8675b849cf9a
Ton <phantom.ness@windowslive.com>
2015-01-07 18:48:19 +0700

    init

---
+++ Quiz/Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Tawatchai Sunarat on 1/7/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "tonAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([tonAppDelegate class]));
+    }
+}
---
+++ Quiz/QuizTests/QuizTests.m
@@ -0,0 +1,34 @@
+//
+//  QuizTests.m
+//  QuizTests
+//
+//  Created by Tawatchai Sunarat on 1/7/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QuizTests : XCTestCase
+
+@end
+
+@implementation QuizTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
