0d46442f50b4eb47605ead1198e3c778f442e795
Ton <phantom.ness@windowslive.com>
2015-01-09 17:58:57 +0700

    init and create HypnosisView

---
+++ Hypnosister/Hypnosister/tonHypnosisterView.m
@@ -0,0 +1,31 @@
+//
+//  tonHypnosisterView.m
+//  Hypnosister
+//
+//  Created by Tawatchai Sunarat on 1/9/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import "tonHypnosisterView.h"
+
+@implementation tonHypnosisterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
