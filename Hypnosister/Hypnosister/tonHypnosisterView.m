4a9923ef7acb7b6ba97db056c76ca5df90ba292a
Ton <phantom.ness@windowslive.com>
2015-01-12 10:39:31 +0700

    gold challence: draw triangle and fill gradient

--- Hypnosister/Hypnosister/tonHypnosisterView.m
+++ Hypnosister/Hypnosister/tonHypnosisterView.m
@@ -25,6 +25,11 @@
 - (void)drawRect:(CGRect)rect
 {
     CGRect bounds = self.bounds;
+    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+    CGRect imageRect = CGRectMake(bounds.size.width / 4.0,
+                                  bounds.size.height / 4.0,
+                                  bounds.size.width / 2.0,
+                                  bounds.size.height / 2.0);
     
     // Figure out the center of the bounds rectangle
     CGPoint center;
@@ -48,27 +53,51 @@
     path.lineWidth = 10;
     [[UIColor lightGrayColor] setStroke];
     
-    //draw
+    //draw hypnosister
     [path stroke];
-    
-    //init image
-    CGContextRef currentContext = UIGraphicsGetCurrentContext();
-    UIImage *logoImg = [UIImage imageNamed:@"logo.png"];
-    CGRect imageRect = CGRectMake(bounds.size.width / 4.0,
-                                 bounds.size.height / 4.0,
-                                 bounds.size.width / 2.0,
-                                 bounds.size.height / 2.0);
-    
-    //save context stage
     CGContextSaveGState(currentContext);
     
-    //set blur
+    //init triangle path
+    UIBezierPath *trianglePath = [[UIBezierPath alloc] init];
+    [trianglePath moveToPoint:CGPointMake(center.x, imageRect.origin.y)];
+    [trianglePath addLineToPoint:CGPointMake(imageRect.origin.x,
+                                             imageRect.origin.y + imageRect.size.height + 20)];
+    [trianglePath addLineToPoint:CGPointMake(imageRect.origin.x + imageRect.size.width,
+                                             imageRect.origin.y + imageRect.size.height + 20)];
+    
+    [trianglePath closePath];
+    [trianglePath addClip];
+    
+    
+    //make gradient
+    CGFloat locations[2] = { 0.0, 1.0};
+    CGFloat components[8] = { 0.0, 1.0, 0.0, 1.0, // start color is green
+                            1.0, 1.0, 0.0, 1.0 }; // end color is yellow
+    
+    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorspace, components, locations, 2);
+    
+    CGPoint startPoint = CGPointMake(center.x, imageRect.origin.y);
+    CGPoint endPoint = CGPointMake(center.x, imageRect.origin.y + imageRect.size.height + 20);
+    CGContextDrawLinearGradient(currentContext, gradient, startPoint, endPoint, 0);
+    
+    //fill gradient
+    CGGradientRelease(gradient);
+    CGColorSpaceRelease(colorspace);
+    
+    CGContextRestoreGState(currentContext);
+    
+    //save context state before set shadow
+    CGContextSaveGState(currentContext);
+    
+    //set shadow
     CGContextSetShadow(currentContext, CGSizeMake(4, 7), 3);
     
     //draw blur image
+    UIImage *logoImg = [UIImage imageNamed:@"logo.png"];
     [logoImg drawInRect: imageRect];
     
-    //finish blur
+    //retore stage
     CGContextRestoreGState(currentContext);
 
 }
