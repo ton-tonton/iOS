b2b72838528252bffe4ce7391a362b05d2922352
Ton <phantom.ness@windowslive.com>
2015-01-15 10:44:07 +0700

    add Chapter 1: Quiz

---
+++ Chapter 1/Quiz/Quiz/tonAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  tonAppDelegate.h
+//  Quiz
+//
+//  Created by Tawatchai Sunarat on 1/7/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tonAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chapter 1/Quiz/Quiz/tonQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  tonQuizViewController.h
+//  Quiz
+//
+//  Created by Tawatchai Sunarat on 1/7/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tonQuizViewController : UIViewController
+
+@end
