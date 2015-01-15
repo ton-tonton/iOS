d60b9938d26aee96455c846e62e07b577244e904
Ton <phantom.ness@windowslive.com>
2015-01-15 10:46:58 +0700

    add Chapter 4: Hypnosister

---
+++ Chapter 4/Hypnosister/Hypnosister/TONAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  tonAppDelegate.h
+//  Hypnosister
+//
+//  Created by Tawatchai Sunarat on 1/9/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TONAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
