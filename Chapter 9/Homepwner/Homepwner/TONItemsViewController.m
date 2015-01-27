36436a2d434f9e3ac34174eb2357849b40dffcf0
Ton <phantom.ness@windowslive.com>
2015-01-27 13:34:24 +0700

    Add Chapter: 9

---
+++ Chapter 9/Homepwner/Homepwner/TONItemsViewController.m
@@ -0,0 +1,125 @@
+//
+//  TONItemsViewController.m
+//  Homepwner
+//
+//  Created by Tawatchai Sunarat on 1/24/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import "TONItemsViewController.h"
+#import "TONItem.h"
+#import "TONItemStore.h"
+
+@interface TONItemsViewController ()
+
+@property (nonatomic, strong) IBOutlet UIView *headerView;
+
+@end
+
+@implementation TONItemsViewController
+
+-(instancetype)init
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    
+    /*if (self) {
+        
+    }*/
+    
+    return self;
+}
+
+-(instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    
+    UIView *header = self.headerView;
+    [self.tableView setTableHeaderView:header];
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    CGRect frame = [[UIScreen mainScreen] bounds];
+    frame.origin.y += 20;
+    frame.size.height -= 20;
+    
+    self.tableView.frame = frame;
+}
+
+-(UIView *)headerView
+{
+    if (!_headerView) {
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
+    }
+    
+    return _headerView;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[TONItemStore sharedStore] allItems] count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView
+        cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    NSArray *items = [[TONItemStore sharedStore] allItems];
+    TONItem *item = items[indexPath.row];
+    cell.textLabel.text = [item description];
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView
+        commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+        forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSArray *items = [[TONItemStore sharedStore] allItems];
+        TONItem *item = items[indexPath.row];
+        [[TONItemStore sharedStore] removeItem:item];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
+}
+
+-(void)tableView:(UITableView *)tableView
+        moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+        toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    [[TONItemStore sharedStore] moveItemAtIndex:sourceIndexPath.row toIndex:destinationIndexPath.row];
+    
+}
+
+-(IBAction)addNewItem:(id)sender
+{
+    TONItem *newItem = [[TONItemStore sharedStore] createItem];
+    NSInteger lastRow = [[[TONItemStore sharedStore] allItems] indexOfObject:newItem];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:lastRow inSection:0];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+    
+}
+
+-(IBAction)toggleEditingMode:(id)sender
+{
+    if (self.isEditing) {
+        
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        [self setEditing:NO animated:YES];
+        
+    } else {
+        
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        [self setEditing:YES animated:YES];
+    }
+}
+
+@end
