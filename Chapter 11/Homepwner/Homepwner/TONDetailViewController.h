55c19bd419ed541fd99ff9cc6bbb9a60b5d8da5f
Ton <phantom.ness@windowslive.com>
2015-01-29 15:22:08 +0700

    add Chapter 11

---
+++ Chapter 11/Homepwner/Homepwner/TONDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  TONDetailViewController.h
+//  Homepwner
+//
+//  Created by Tawatchai Sunarat on 1/28/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TONItem;
+
+@interface TONDetailViewController : UIViewController
+
+@property (nonatomic, strong) TONItem *item;
+
+@end
