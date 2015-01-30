9d47d47756884952fdd9657ac8e672d34357ff37
manit <ss.manit@gmail.com>
2015-01-31 00:04:08 +0700

    Chapter 13: Add Silver Challenge

--- Chapter 13/TouchTracker/TouchTracker/TONDrawView.m
+++ Chapter 13/TouchTracker/TouchTracker/TONDrawView.m
@@ -61,6 +61,7 @@
                                action:@selector(moveLine:)];
         self.moveRecognizer.delegate = self;
         self.moveRecognizer.cancelsTouchesInView = NO;
+        self.moveRecognizer.delaysTouchesBegan = YES;
         [self addGestureRecognizer:self.moveRecognizer];
     }
     
@@ -183,6 +184,7 @@
 }
 
 #pragma mark - Gesture recognize protocol
+
 -(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
 {
     if (gestureRecognizer == self.moveRecognizer) {
@@ -248,6 +250,12 @@
 -(void)moveLine:(UIPanGestureRecognizer *)gr
 {
     if (!self.selectedLine) {
+        return;
+    }
+    
+    if ([[UIMenuController sharedMenuController] isMenuVisible]) {
+        [[UIMenuController sharedMenuController] setMenuVisible:NO animated:YES];
+        self.selectedLine = nil;
         return;
     }
     
