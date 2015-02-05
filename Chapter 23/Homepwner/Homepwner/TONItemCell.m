14689c0a52c70fa55fafdc521f917aad7d44719b
Ton <phantom.ness@windowslive.com>
2015-02-05 17:36:22 +0700

    add Chapter 23 + coreData

---
+++ Chapter 23/Homepwner/Homepwner/TONItemCell.m
@@ -0,0 +1,83 @@
+//
+//  TONItemCell.m
+//  Homepwner
+//
+//  Created by Tawatchai Sunarat on 2/4/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import "TONItemCell.h"
+
+@interface TONItemCell ()
+
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *imageViewHeightConstraint;
+
+@end
+
+@implementation TONItemCell
+
+-(IBAction)showImage:(id)sender
+{
+    if (self.actionBlock) {
+        
+        self.actionBlock();
+    }
+}
+
+#pragma mark - dynamic type size
+
+-(void)updateInterfaceForDynamicTypeSize
+{
+    UIFont *font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    
+    self.nameLabel.font = font;
+    self.serialNumberLabel.font = font;
+    self.valueLabel.font = font;
+    
+    static NSDictionary *imgSize = nil;
+    
+    if (!imgSize) {
+        imgSize = @{ UIContentSizeCategoryExtraSmall : @40,
+                      UIContentSizeCategorySmall : @40,
+                      UIContentSizeCategoryMedium : @40,
+                      UIContentSizeCategoryLarge : @40,
+                      UIContentSizeCategoryExtraLarge : @45,
+                      UIContentSizeCategoryExtraExtraLarge : @55,
+                      UIContentSizeCategoryExtraExtraExtraLarge : @65
+                     };
+    }
+    
+    NSString *userSize = [[UIApplication sharedApplication] preferredContentSizeCategory];
+    
+    NSNumber *imageSize = imgSize[userSize];
+    self.imageViewHeightConstraint.constant = imageSize.floatValue;
+}
+
+-(void)awakeFromNib
+{
+    [self updateInterfaceForDynamicTypeSize];
+    
+    NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
+    [defaultCenter addObserver:self
+                      selector:@selector(updateInterfaceForDynamicTypeSize)
+                          name:UIContentSizeCategoryDidChangeNotification
+                        object:nil];
+    
+    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self.thumbnailView
+                                                                  attribute:NSLayoutAttributeHeight
+                                                                  relatedBy:NSLayoutRelationEqual
+                                                                     toItem:self.thumbnailView
+                                                                  attribute:NSLayoutAttributeWidth
+                                                                 multiplier:1
+                                                                   constant:0];
+    
+    [self.thumbnailView addConstraint:constraint];
+}
+
+-(void)dealloc
+{
+    NSNotificationCenter *def = [NSNotificationCenter defaultCenter];
+    [def removeObserver:self];
+}
+
+@end
---
+++ Chapter 23/Homepwner/Homepwner/TONItemStore.m
@@ -0,0 +1,187 @@
+//
+//  TONItemStore.m
+//  Homepwner
+//
+//  Created by Tawatchai Sunarat on 1/24/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import "TONItemStore.h"
+#import "TONItem.h"
+#import "TONImageStore.h"
+
+@import CoreData;
+
+@interface TONItemStore ()
+
+@property (nonatomic) NSMutableArray *privateItems;
+@property (nonatomic, strong) NSMutableArray *allAssetType;
+@property (nonatomic, strong) NSManagedObjectContext *context;
+@property (nonatomic, strong) NSManagedObjectModel *model;
+
+@end
+
+@implementation TONItemStore
+
+#pragma mark - initail
+
++(instancetype)sharedStore
+{
+    static TONItemStore *sharedStore = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        sharedStore = [[self alloc] initPrivate];
+    });
+    
+    return sharedStore;
+}
+
+-(instancetype)init
+{
+    @throw [NSException exceptionWithName:@"Singleton"
+                                   reason:@"Use +[TONItemstore sharedStore]"
+                                 userInfo:nil];
+    return nil;
+}
+
+-(instancetype)initPrivate
+{
+    self = [super init];
+    if (self) {
+        
+        _model = [NSManagedObjectModel mergedModelFromBundles:nil];
+        NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:_model];
+        NSString *path = [self itemArchivePath];
+        NSURL *storeUrl = [NSURL fileURLWithPath:path];
+        NSError *err = nil;
+        
+        if (
+            ![psc addPersistentStoreWithType:NSSQLiteStoreType
+                               configuration:nil
+                                         URL:storeUrl
+                                     options:nil
+                                       error:&err]
+            ) {
+            @throw [NSException exceptionWithName:@"OpenFailure" reason:@"error localizedDescription" userInfo:nil];
+        }
+        
+        _context = [[NSManagedObjectContext alloc] init];
+        _context.persistentStoreCoordinator = psc;
+        [self loadAllItems];
+        
+    }
+    
+    return self;
+}
+
+#pragma mark - handle function
+
+-(NSArray *)allItems
+{
+    return self.privateItems;
+}
+
+-(TONItem *)createItem
+{
+    double order;
+    
+    if ([self.allItems count] == 0) {
+        
+        order = 1.0;
+    } else {
+        
+        order = [[self.privateItems lastObject] orderingValue] + 1.0;
+    }
+    
+    NSLog(@"Adding after %d items, order = %.2f", [self.privateItems count], order);
+    
+    TONItem *item = [NSEntityDescription insertNewObjectForEntityForName:@"TONItem"
+                                                  inManagedObjectContext:self.context];
+    
+    item.serialNumber = [TONItem randomSerialNumber];
+    item.orderingValue = order;
+    
+    [self.privateItems addObject:item];
+    return item;
+}
+
+-(void)removeItem:(TONItem *)item
+{
+    [[TONImageStore sharedStroe] deleteImageForKey:item.imageKey];
+    [self.context deleteObject:item];
+    [self.privateItems removeObjectIdenticalTo:item];
+}
+
+-(void)moveItemAtIndex:(NSInteger)fromIndex toIndex:(NSInteger)toIndex
+{
+    TONItem *moveItem = [self.privateItems objectAtIndex:fromIndex];
+    [self.privateItems removeObjectAtIndex:fromIndex];
+    [self.privateItems insertObject:moveItem atIndex:toIndex];
+    
+    double lowerBound = 0.0;
+    double upperBound = 0.0;
+    
+    if (toIndex > 0) {
+        lowerBound = [self.privateItems[(toIndex - 1)] orderingValue];
+    } else {
+        lowerBound = [self.privateItems[1] orderingValue] - 2.0;
+    }
+    
+    if (toIndex < [self.privateItems count] - 1) {
+        upperBound = [self.privateItems[(toIndex + 1)] orderingValue];
+    } else {
+        upperBound = [self.privateItems[(toIndex - 1)] orderingValue] + 2.0;
+    }
+    
+    double nowOrderValue = (lowerBound + upperBound) / 2.0;
+    NSLog(@"moving to order %f", nowOrderValue);
+    moveItem.orderingValue = nowOrderValue;
+}
+
+#pragma mark - read-write file
+
+-(NSString *)itemArchivePath
+{
+    NSArray *directories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *directory = [directories firstObject];
+    return [directory stringByAppendingPathComponent:@"store.data"];
+}
+
+-(void)loadAllItems
+{
+    if (!_privateItems) {
+        
+        NSFetchRequest *req = [[NSFetchRequest alloc] init];
+        NSEntityDescription *e = [NSEntityDescription entityForName:@"TONItem"
+                                             inManagedObjectContext:self.context];
+        req.entity = e;
+        
+        NSSortDescriptor *sortdes = [NSSortDescriptor sortDescriptorWithKey:@"orderingValue"
+                                                                  ascending:YES];
+        req.sortDescriptors = @[sortdes];
+        
+        NSError *err;
+        NSArray *result = [self.context executeFetchRequest:req error:&err];
+        
+        if (!result) {
+            [NSException raise:@"Fetch failed" format:@"Reason: %@", [err localizedDescription]];
+        }
+        
+        self.privateItems = [[NSMutableArray alloc] initWithArray:result];
+    }
+}
+
+-(BOOL)saveChanges
+{
+    NSError *err;
+    BOOL successful = [_context save:&err];
+    
+    if (!successful) {
+        NSLog(@"Error saving:%@", [err localizedDescription]);
+    }
+    
+    return successful;
+}
+
+@end
