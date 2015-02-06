7fcc47ef98fb7c31208f84aceb2d2ef404d6937f
Ton <phantom.ness@windowslive.com>
2015-02-06 17:32:28 +0700

    add Chapter 25

---
+++ Chapter 25/Homepwner/Homepwner/TONAssetTypeViewController.h
@@ -0,0 +1,17 @@
+//
+//  TONAssetTypeViewController.h
+//  Homepwner
+//
+//  Created by Tawatchai Sunarat on 2/5/2558.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TONItem;
+
+@interface TONAssetTypeViewController : UITableViewController
+
+@property (nonatomic, strong) TONItem *item;
+
+@end
